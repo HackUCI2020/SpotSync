548504db3afb14dace68a0a3ece3f13b5896a6ae
William Sun <sunwilliam@gmail.com>
2020-02-01 14:42:40 -0800

    Server can recieve query from frontend

---
+++ PlaylistParser/PlaylistParser.py
@@ -0,0 +1,4 @@
+def parse(input1, input2):
+    print(input1)
+    print(input2)
+    return ["song"+str(i) for i in range(0, 10)]
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from wtforms import Form, TextField, validators
+
+import PlaylistParser.PlaylistParser as PlaylistParser
 
 app = Flask(__name__)
 
 
+class QueryForm(Form):
+    seed = TextField('Seed:', validators=[validators.required()])
+    songs = TextField('Songs:', validators=[validators.required()])
+
+
 @app.route('/')
 @app.route('/index')
-def index():
-    return "hello world"
+@app.route('/form', methods=["GET", "POST"])
+def redirect_from_seed():
+    form = QueryForm(request.form)
+    if request.method == 'POST':
+        seed = request.form['seed']
+        songs = request.form['songs']
+        return render_template('return-query.html', content=PlaylistParser.parse(seed, songs))
+    else:
+        return render_template('form.html', form=form)
+
+
+def results_from_seed(songs):
+    return render_template('return-query.html', content=songs)
 
 
 if __name__ == "__main__":
